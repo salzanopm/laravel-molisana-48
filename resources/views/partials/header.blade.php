d65df6a3844ca02e995441b9f698c14a1464da11
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2022-02-09 12:28:21 +0100

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,21 @@
 <header>
-    Ciao sono l'header
+    <div>
+        <img class="logo" src="{{ asset('images/logo.png') }}" alt="Molisana logo">
+    </div>
+    
+    <nav>
+        <ul class="menu">
+            <li>
+                <a href="">Home</a>
+            </li>
+
+            <li class="current">
+                <a href="">Prodotti</a>
+            </li>
+
+            <li>
+                <a href="">News</a>
+            </li>
+        </ul>
+    </nav>
 </header>
